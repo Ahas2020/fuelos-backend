2d5020285475bc046b6e035073701b6cca04da8e
Ahmed salem Ameur hadad <61189059+Ahas2020@users.noreply.github.com>
2026-06-16 12:25:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const routes = require('./src/routes');
+const routes = require('./index');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
